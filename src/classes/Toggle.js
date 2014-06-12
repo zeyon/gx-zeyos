0ae45f39812c567024e86a8d7107c96435ee1453
Peter Haider <peter.haider@zeyon.net>
2014-06-12 14:42:21 +0200

    Fix DOM issue with gx.zeyos.Toggle

--- src/classes/Toggle.js
+++ src/classes/Toggle.js
@@ -25,43 +25,49 @@ gx.zeyos.Toggle = new Class({
 		this.parent(display, options);
 
 		var root = this;
-		this._display.root.addClass('tgl');
-		this._display.root.addEvent('click', function() {
+		if (this._display.root.get('tag') == 'fieldset') {
+			this._display.fieldset = this._display.root;
+		} else {
+			this._display.fieldset = new Element('fieldset', {'class': 'm_r-10'}).inject(this._display.root, 'top');
+		}
+
+		this._display.fieldset.addClass('tgl');
+		this._display.fieldset.addEvent('click', function() {
 			root.toggle();
 		});
 
 		if ( this.options.on )
-			this._display.root.addClass('act');
+			this._display.fieldset.addClass('act');
 	},
 
 	getState: function() {
-		if ( this._display.root.hasClass('act') )
+		if ( this._display.fieldset.hasClass('act') )
 			return true;
 		else
 			return false;
 	},
 
 	getValue: function() {
-		if ( this._display.root.hasClass('act') )
+		if ( this._display.fieldset.hasClass('act') )
 			return this.options.value;
 		else
 			return false;
 	},
 
 	toggle: function() {
-		if ( this._display.root.hasClass('act') )
+		if ( this._display.fieldset.hasClass('act') )
 			this.setUnchecked();
 		else
 			this.setChecked();
 	},
 
 	setChecked: function() {
-		this._display.root.addClass('act');
+		this._display.fieldset.addClass('act');
 		this.fireEvent('check');
 	},
 
 	setUnchecked: function() {
-		this._display.root.removeClass('act');
+		this._display.fieldset.removeClass('act');
 		this.fireEvent('uncheck');
 	}
 });
